a40c6846198f92fd5c13c42477b1ab13e104d053
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2022-01-09 20:00:30 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/marginals/navbar/Navbar";
 import Home from "./screens/Home";
-import Carousel from "./components/carousel/carousel";
-import { blogs } from "./content/Navigation";
+// import Carousel from "./components/carousel/carousel";
+// import { blogs } from "./content/Navigation";
 import Blogs from "./pages/blogs/Blogs";
 import datas from "./components/cards/data";
-import Maps from "./components/maps/Maps";
-import Communities from "./pages/communities/Communities";
+// import Maps from "./components/maps/Maps";
+// import Communities from "./pages/communities/Communities";
 import { BasicTable } from "./components/portal/BasicTable";
 
 import Counsel from "./pages/counselling/counsel";
 import Helpline from "./pages/helpline/helpline";
 import Coming from "./components/coming/coming";
-import Callblogs from "./pages/blogs/Callblogs";
+// import Callblogs from "./pages/blogs/Callblogs";
 function App() {
   return (
     <Router>
@@ -28,7 +28,7 @@ function App() {
         {/* <Route exact path="/blogs" element={<Callblogs />} /> */}
 
         <Route path="/portal" element={<BasicTable />} />
-        <Route exact path="/community" element={<Communities />} />
+        {/* <Route exact path="/community" element={<Communities />} /> */}
 
         <Route exact path="/issues/counselling" element={<Counsel />} />
         <Route exact path="/issues/helpline" element={<Helpline />} />
