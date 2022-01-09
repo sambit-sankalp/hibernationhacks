4d0940c98567f6a7a115eb998bd5930390c13894
Akash <akashsahu006@gmail.com>
2022-01-09 18:46:29 +0530

    final

--- src/pages/communities/data.js
+++ src/pages/communities/data.js
@@ -3,20 +3,20 @@ export default [
     id: 1,
     heading: "Hii",
     link: "https://dare2compete.com/creative-cultural-event/mylifemantra-from-my-mother-manav-rachna-university-mru-faridabad-164743",
-    img: "https://res.cloudinary.com/dcbco6i8s/image/upload/v1641707521/mom_nownty.jpg",
+    img: "https://res.cloudinary.com/dcbco6i8s/image/upload/v1641732867/Screenshot_2022-01-09_181705_u6fyoi.png",
   },
   {
     id: 2,
 
     heading: "Hii",
     link: "https://dare2compete.com/creative-cultural-event/mylifemantra-from-my-mother-manav-rachna-university-mru-faridabad-164743",
-    img: "https://res.cloudinary.com/dcbco6i8s/image/upload/v1641707539/mom3_kugxqa.jpg",
+    img: "https://res.cloudinary.com/dcbco6i8s/image/upload/v1641732863/7152_blog-post_hxx8qo.png",
   },
   {
     id: 3,
 
     heading: "Hii",
     link: "https://dare2compete.com/creative-cultural-event/mylifemantra-from-my-mother-manav-rachna-university-mru-faridabad-164743",
-    img: "https://res.cloudinary.com/dcbco6i8s/image/upload/v1641707539/mom2_zbjamf.jpg",
+    img: "https://res.cloudinary.com/dcbco6i8s/image/upload/v1641733717/EN_CSW65_960Wx370H_1_grmx1l.png",
   },
 ];
